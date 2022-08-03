e7e729b2a403b67e89bdcfdda50e126cfa67beb2
jjiinneee <llj2653@gmail.com>
2022-08-03 23:52:59 +0900

    Initial commit

---
+++ src/test/java/com/example/muapi/MuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuApiApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
